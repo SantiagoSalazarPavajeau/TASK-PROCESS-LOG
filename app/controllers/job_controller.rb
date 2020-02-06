8b4dc5c48c0cff861de925ae6b27fe920eef2d6f
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-02-05 20:23:12 -0500

    jobs index route

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -3,6 +3,40 @@ require './config/environment'
 
 class JobsController < ApplicationController
 
+    get '/jobs' do 
+        # index displays all jobs
+        @jobs = Job.all
+        erb :'/jobs/index'
+    end
+
+    get '/jobs/new' do
+        #create new job form
+    end
+
+    post '/jobs' do
+        #create one new job (button action)
+    end
+
+    get '/jobs/:id' do
+        # displays job with :id
+    end
+
+    get '/jobs/:id/edit' do
+        # edit jobs form
+    end
+
+    patch '/jobs/:id' do
+        #modifies job with :id
+    end
+
+    put '/jobs/:id' do
+        # replaces job
+    end
+
+    delete '/jobs/:id' do
+        # deletes job with :id
+    end
+
 
 
 
