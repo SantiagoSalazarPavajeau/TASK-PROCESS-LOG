a1c7e9b9088f965faa70d76d74894c656bdc12c2
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-02-05 21:12:44 -0500

    clarify user parameters and start seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+
+User.create(name: 'Santi', email: 'santi@gmail.com', password: 'password')
+Job.create(name: 'Order Administrator')
+Task.create(description: 'Put order into system')
+Process.create(name: 'Guitar Order')
