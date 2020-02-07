10e54e604703c18910c2dbb90ae0081fa587f4a4
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-02-07 14:44:59 -0500

    add task index by global processes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,14 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
-  get '/tasks' do
+  get '/tasks_by_job' do
     @jobs = Job.all
+    erb :'/tasks/index_by_job'
+  end
+
+  get '/tasks_by_global_processes' do
     @global_processes = GlobalProcess.all
-    erb :'/tasks/index'
+    erb :'/tasks/index_by_global_processes'
   end
 
   
