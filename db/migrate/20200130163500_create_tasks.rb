6cb88e5558687eb828769c72105a58d80ea46dc6
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-02-09 21:41:35 -0500

    change tasks description to text

--- db/migrate/20200130163500_create_tasks.rb
+++ db/migrate/20200130163500_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :description
+      t.text :description
       t.integer :job_id
       t.integer :global_process_id
       t.integer :order
